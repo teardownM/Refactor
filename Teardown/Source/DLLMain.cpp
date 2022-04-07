74f741875c13129c37ca49c25fbfccf482a9d1fe
vulcan-dev <vitexuhd@gmail.com>
2022-04-07 18:14:43 +0100

    Modify menu on start and revert on exit

--- Teardown/Source/DLLMain.cpp
+++ Teardown/Source/DLLMain.cpp
@@ -3,8 +3,21 @@
 #include "Globals.h"
 #include "Console.h"
 #include "detours.h"
+#include <filesystem>
+#include "Teardown/Menu.h"
 
 __declspec(dllexport) int __stdcall _(void) { return 0; }
+
+void Shutdown(HMODULE hModule, DWORD reason) {
+    if (!hModule)
+        FreeLibraryAndExitThread(GetModuleHandle(nullptr), reason);
+
+    Teardown::Menu::Revert();
+#ifdef DEBUG
+    ::Utilities::Console::Close();
+#endif
+    FreeLibraryAndExitThread(hModule, reason);
+}
 
 DWORD WINAPI StartRoutine([[maybe_unused]] HMODULE hModule) {
 #ifdef DEBUG
@@ -17,31 +30,34 @@ DWORD WINAPI StartRoutine([[maybe_unused]] HMODULE hModule) {
     #define LOG_ERROR(...)
     #define LOG_FATAL(...)
 #endif
+    bool bRunning = true;
 
-    LOG_DEBUG("Starting...");
-	
     g_Module = GetModuleHandle(nullptr);
 
-    LOG_DEBUG("Getting Addresses...");
-    Teardown::GetFunctionAddresses();
+    Teardown::Path = std::filesystem::current_path().string();
+    if (Teardown::Menu::Set() != 0) {
+        LOG_ERROR("Failed to create menu");
+        Shutdown(g_Module, 1);
+        return 1;
+    }
 
-#ifdef DEBUG
-    ::Utilities::Console::Close();
-#endif
+/*    LOG_DEBUG("Getting Addresses...");
+    Teardown::GetFunctionAddresses();*/
 
-    FreeLibraryAndExitThread(hModule, 0);
+//
+    return 0;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, [[maybe_unused]] LPVOID lpReserved) {
-    switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
-        DisableThreadLibraryCalls(hModule);
-        CloseHandle(CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(StartRoutine), hModule, 0, nullptr));
-        break;
-    case DLL_PROCESS_DETACH:
-        break;
-    default:
-        break;
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ulReason, [[maybe_unused]] LPVOID lpReserved) {
+    switch (ulReason) {
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls(hModule);
+            CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(StartRoutine), hModule, 0, nullptr);
+            break;
+        case DLL_PROCESS_DETACH: {
+            Shutdown(g_Module, 0);
+            break;
+        } default: break;
     }
 
     return TRUE;
