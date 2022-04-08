eb4b4ae1aecc154bb50c83560f2a8915ecd959f0
vulcan-dev <vitexuhd@gmail.com>
2022-04-08 03:51:23 +0100

    Added File Logger

--- Teardown/Include/Logger.h
+++ Teardown/Include/Logger.h
@@ -11,26 +11,6 @@ public:
     static void Initialize();
     static Ref<spdlog::logger>& GetLogger() { return m_Logger; }
 
-    template<typename... Args>
-    static void Error(fmt::format_string<Args...> fmt, Args &&...args) {
-        #ifdef _DEBUG
-                m_Logger->error(fmt, std::forward<Args>(args)...);
-        #else
-                auto msg = fmt::format(fmt, std::forward<Args>(args)...);
-                MessageBoxA(nullptr, msg.c_str(), "Error", MB_OK | MB_ICONERROR);
-        #endif
-    }
-
-    template<typename... Args>
-    static void Critical(fmt::format_string<Args...> fmt, Args &&...args) {
-        #ifdef _DEBUG
-                m_Logger->critical(fmt, std::forward<Args>(args)...);
-        #else
-                auto msg = fmt::format(fmt, std::forward<Args>(args)...);
-                MessageBoxA(nullptr, msg.c_str(), "Fatal", MB_OK | MB_ICONERROR);
-        #endif
-    }
-    
 private:
     static Ref<spdlog::logger> m_Logger;
 };
@@ -38,5 +18,5 @@ private:
 #define LOG_DEBUG(...) ::Logger::GetLogger()->debug(__VA_ARGS__)
 #define LOG_INFO(...) ::Logger::GetLogger()->info(__VA_ARGS__)
 #define LOG_WARN(...) ::Logger::GetLogger()->warn(__VA_ARGS__)
-#define LOG_ERROR(...) ::Logger::Error(__VA_ARGS__)
-#define LOG_CRITICAL(...) ::Logger::Critical(__VA_ARGS__)
+#define LOG_ERROR(...) ::Logger::GetLogger()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...) ::Logger::GetCoreLogger()->critical(__VA_ARGS__)
--- Teardown/Source/Logger.cpp
+++ Teardown/Source/Logger.cpp
@@ -1,19 +1,33 @@
 #include "Logger.h"
 
+#include <spdlog/async_logger.h>
+#include <spdlog/async.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include "spdlog/sinks/rotating_file_sink.h"
 
 Ref<spdlog::logger> Logger::m_Logger;
 
 void Logger::Initialize() {
-	std::vector<spdlog::sink_ptr> logSinks;
-	logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    spdlog::init_thread_pool(8192, 1);
+    auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt >();
+    auto fileSink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("TDMLogs/Log.txt", 1024*1024*10, 3);
 
-	m_Logger = std::make_shared<spdlog::logger>("TDM", begin(logSinks), end(logSinks));
-	spdlog::register_logger(m_Logger);
+#ifdef _DEBUG
+    consoleSink->set_level(spdlog::level::debug);
+#else
+    consoleSink->set_level(spdlog::level::info);
+#endif
 
-#ifdef DEBUG
+    fileSink->set_level(spdlog::level::info);
+
+    std::vector<spdlog::sink_ptr> sinks {consoleSink, fileSink};
+    m_Logger = std::make_shared<spdlog::async_logger>("TDM", sinks.begin(), sinks.end(), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
+
+#ifdef _DEBUG
+    m_Logger->flush_on(spdlog::level::debug);
     m_Logger->set_level(spdlog::level::debug);
 #else
+    m_Logger->flush_on(spdlog::level::info);
     m_Logger->set_level(spdlog::level::info);
 #endif
 }
